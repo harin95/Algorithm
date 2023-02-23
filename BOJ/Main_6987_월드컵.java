1473c3414f491587e7ddcc7da898737c5d895010
harin95 <harin4rang@gmail.com>
2023-02-23 09:05:27 +0900

    now solving

---
+++ BOJ/Main_6987_월드컵.java
@@ -0,0 +1,24 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_6987_월드컵 {
+    public static void main(String[] args) throws IOException {
+
+        int[][] worldCup = new int[6][6];
+        int[][] result = new int[6][3];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<6; i++){
+            for(int j=0; j<3; j++){
+
+            }
+        }
+
+    }
+}
