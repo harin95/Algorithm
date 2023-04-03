7163f643eec509d03b382219d681813d3991506c
정하린 <harin4rang@gmail.com>
2023-04-03 15:52:46 +0900

    BOJ

--- BOJ/BJ19623_회의실배정4.java
+++ BOJ/BJ19623_회의실배정4.java
@@ -1,11 +1,11 @@
-package Algorithm.BOJ;
+package BOJ;
 
 import java.util.*;
 import java.io.*;
 
 public class BJ19623_회의실배정4 {
 
-    private static class Meeting implements Comparable<Meeting>{
+    private static class Meeting implements Comparable<Meeting> {
         int start, end, people;
 
         public Meeting(int start, int end, int people) {
@@ -16,8 +16,8 @@ public class BJ19623_회의실배정4 {
 
         @Override
         public int compareTo(Meeting o) {
-            return Integer.compare(this.end, o.end);
-        }
+            return Integer.compare(this.start, o.start);
+        }   //시작시간 기준 정렬
     }
 
     public static void main(String[] args) throws IOException {
@@ -28,52 +28,51 @@ public class BJ19623_회의실배정4 {
         int n = Integer.parseInt(br.readLine());
         List<Meeting> list = new ArrayList<>();
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            list.add(new Meeting(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
-                    Integer.parseInt(st.nextToken())));
-
-            Collections.sort(list);
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int people = Integer.parseInt(st.nextToken());
+            list.add(new Meeting(start, end, people));
         }
+
+        Collections.sort(list);
 
         int[] dp = new int[n];
-        dp[0] = list.get(0).people;
-        int max = dp[0];
+        dp[n-1] = list.get(n-1).people;
 
-        for(int i=1; i<n; i++){
-            int start = list.get(i).start;
-            //이분탐색
-            //끝나는 시간 중에서 start보다 작은 가장 큰 값의 인덱스
-            int j = binarySerach(list, start);
+        /*
+        * dp배열 : i ~ n-1 회의의 최대 인원
+        * 회의 i를 선택했을 때 : 회의 i의 인원 + 회의 i의 끝나는 시간보다 큰 시간 중 가장 작은 시간 회의의 인원
+        * 회의 i를 선택하지 않았을 때 : i+1 ~ n-1 회의의 최대 인원*/
 
-            if(j != -1){
-                dp[i] = dp[j] + list.get(i).people;  //있으면 이전 인원수에 더함
-            }
-            else{
-                dp[i] = list.get(i).people;     //없으면 자신 그대로
-            }
-            max = Math.max(max, dp[i]);     //최대값 갱신
+        for(int i=n-2; i>=0; i--){
+            int key = list.get(i).end;
+            int j = binarySerach(list, key);
+            int cmp = j == n ? list.get(i).people : dp[j] + list.get(i).people;
+            dp[i] = Math.max(dp[i+1], cmp);
         }
-        sb.append(max).append("\n");
+
+        sb.append(dp[0]).append("\n");
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         bw.write(sb.toString());
         bw.close();
     }
 
-    private static int binarySerach(List<Meeting> list, int key){
+    private static int binarySerach(List<Meeting> list, int key) {
+        /*key보다 큰 값 중 가장 작은 값의 인덱스를 반환한다*/
         int low = 0;
         int high = list.size();
 
-        while(low < high){
-            int mid = (low+high)/2;
+        while (low < high) {
+            int mid = (low + high) / 2;
 
-            if(list.get(mid).end >= key){
+            if (list.get(mid).start > key) {
                 high = mid;
-            }
-            else{
-                low = mid+1;
+            } else {
+                low = mid + 1;
             }
         }
-        return high-1;
+        return high;
     }
 }
