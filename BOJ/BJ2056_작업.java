d1ed3d5400d62ac1221fc7ddf306dd02ebf8b648
정하린 <harin4rang@gmail.com>
2023-04-13 17:48:35 +0900

    BOJ

---
+++ BOJ/BJ2056_작업.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.*;
+import java.io.*;
+
+public class BJ2056_작업 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int answer = 0;
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+}
