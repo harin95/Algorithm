5a0080de0c010a79892a8630f835d9f1e3f1f998
harin95 <harin4rang@gmail.com>
2023-05-01 11:59:40 +0900

    BOJ solving

---
+++ BOJ/BJ8980_택배.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+public class BJ8980_택배 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int m = Integer.parseInt(br.readLine());
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int post = Integer.parseInt(st.nextToken());
+        }
+    }
+}
