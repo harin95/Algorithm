4a4fee188cd62e8b91fe1ec3688124701ef7bc2f
harin95 <harin4rang@gmail.com>
2023-06-29 21:44:25 +0900

    BOJ

---
+++ BOJ/BJ13699_점화식.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.nio.Buffer;
+import java.util.Scanner;
+
+public class BJ13699_점화식 {
+
+    private static long[] numbers;
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+
+        numbers = new long[36];
+        numbers[0] = 1;
+
+        for(int i=1; i<=35; i++){
+
+            long num = 0;
+
+            for(int j=0; j<i; j++){
+                num += numbers[j]*numbers[i-j-1];
+            }
+
+            numbers[i] = num;
+        }
+
+        System.out.println(numbers[input]);
+    }
+}
