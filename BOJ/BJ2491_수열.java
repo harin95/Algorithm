b77e02c48de3eb8411aa10e1a9feaa2f9946570f
harin95 <76214729+harin95@users.noreply.github.com>
2023-07-06 13:16:17 +0900

    BJ2491_수열_주석추가

--- BOJ/BJ2491_수열.java
+++ BOJ/BJ2491_수열.java
@@ -9,12 +9,16 @@ public class BJ2491_수열 {
         int n = Integer.parseInt(br.readLine());
         int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        int lenInc = 1;
-        int lenDec = 1;
+        int lenInc = 1;    //증가하는 수열 길이
+        int lenDec = 1;    //감소하는 수열 길이
         int max = 1;
 
+        /**
+        * 반복문 돌며 증가하는 수열, 감소하는 수열 길이(같은값도 포함) 동시에 체크
+        * 최대값 갱신
+        */
         for(int i=1; i<n; i++){
-            if(input[i-1] < input[i]) lenInc++;
+            if(input[i-1] <= input[i]) lenInc++;
             else lenInc = 1;
             max = Math.max(max, lenInc);
 
